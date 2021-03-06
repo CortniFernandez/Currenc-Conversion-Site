5d312f536acf46b930c6afbe435f49cda762b4e0
Cortni <cortni.fernandez@gmail.com>
2021-03-06 10:56:50 -0500

    Table page displaying but not updating yet

--- src/Currency-Table.js
+++ src/Currency-Table.js
@@ -1,22 +1,23 @@
 import React from 'react';
 
-function CurrTable() {
+const CurrTable = (props) => {
+  const { base, rates } = props;
+  if (!rates) {
+    return null;
+  }
+
   return (
     <div className="te-container">
       <div className="row">
         <div className="col-lg-4 offset-lg-4 col-md-8 offset-md-2">
           <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">1 <span><select>(curr)</select></span> =</th>
-                <th></th>
-              </tr>
-            </thead>
             <tbody>
-                <tr>
-                  <td align="right">rate</td>
-                  <td>currency</td>
+              {rates.map(currency =>
+                <tr key={currency.acronym}>
+                  <td align="right">{currency.rate}</td>
+                  <td>{currency.acronym}</td>
                 </tr>
+              )}
             </tbody>
           </table>
         </div>
--- src/Table-Exchange.js
+++ src/Table-Exchange.js
@@ -1,16 +1,64 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './Table-Exchange.css';
+import currencies from './currencies';
 import CurrTable from './Currency-Table';
 
-const LatestRates = 'https://alt-exchange-rate.herokuapp.com/latest'
+class TEApp extends React.Component {
+  constructor () {
+    super();  
+    this.state = {
+      base: 'USD',
+      rates: null,
+    }
+  }
 
-function TEApp() {
-  return (
-    <div className="App">
-      <h1 className="title">Table Exchange</h1>
-      <CurrTable />
-    </div>
-  );
+  componentDidMount() {
+    this.getRatesData(this.state.base);
+  }
+
+  changeBase = (e) => {
+    this.setState({ base: e.target.value });
+  }
+  
+  getRatesData = (base) => {
+    fetch(`https://alt-exchange-rate.herokuapp.com/latest?base=${base}`)
+      .then(response => response.json())
+      .then(data => {
+        if (data.error) {
+          throw new Error(data.error);
+        }
+
+        const rates = Object.keys(data.rates)
+        .filter(acronym => acronym !== base)
+        .map(acronym => ({
+          acronym,
+          rate: data.rates[acronym],
+          name: currencies[acronym].name,
+          symbol: currencies[acronym].symbol,
+        }))
+
+        this.setState({ rates });
+      })
+      .catch(error => console.error(error.message));
+  }
+
+
+  render () {
+    const { base, rates } = this.state;
+
+    return (
+      <div className="App">
+        <h1 className="title">Table Exchange</h1>
+        <form className="p-3 bg-light form-inline justify-content-center">
+          <h3 className="mb-2"><b className="mr-2">1</b>
+          <select value={base} onChange={this.changeBase} className="form-control form-control-lg mb-2">
+            {Object.keys(currencies).map(currencyAcronym => <option key={currencyAcronym} value={currencyAcronym}>{currencyAcronym}</option>)}
+          </select><b className="ml-2">=</b></h3>
+        </form>
+        <CurrTable base={base} rates={rates} />
+      </div>
+    );
+  }
 }
 
 export default TEApp;
