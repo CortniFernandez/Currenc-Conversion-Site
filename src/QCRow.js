372ac68df28dd27152388150f5816f347c757a2b
Cortni <cortni.fernandez@gmail.com>
2021-03-05 19:37:44 -0500

    Exchange rate math working in QC, but not currencies yet

--- src/QCRow.js
+++ src/QCRow.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './QCRow.css';
 
-function QCRow() {
+
+function QCRow(props) {
+  const {
+    currencyOptions,
+    selectedCurrency,
+    onChangeCurrency,
+    onChangeAmount,
+    amount
+  } = props
   return (
     <div className="qc-row">
-      <input type="number" className="qc-input" />
-      <select className="qc-select">
-        <option value="hi">Hi</option>
+      <input type="number" className="qc-input" value={amount} onChange={onChangeAmount}/>
+      <select className="qc-select" value={selectedCurrency} onChange={onChangeCurrency}>
+        {currencyOptions.map(option => (
+          <option key={option} value={option}>{option}</option>
+        ))}
       </select>
     </div>
   );
--- src/Quick-Conversion.js
+++ src/Quick-Conversion.js
@@ -1,18 +1,71 @@
+import React, { useEffect, useState } from 'react';
 import './Quick-Conversion.css';
 import QCRow from './QCRow';
 
+const LatestRates = 'https://alt-exchange-rate.herokuapp.com/latest'
+
 function QCApp() {
+  const [currencyOptions, setCurrencyOptions] = useState([])
+  const [fromCurrency, setFromCurrency] = useState()
+  const [toCurrency, setToCurrency] = useState()
+  const [exchangeRate, setExchangeRate] = useState()
+  const [amount, setAmount] = useState(1)
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true)
+
+  let toAmount, fromAmount
+  if (amountInFromCurrency) {
+    fromAmount = amount
+    toAmount = amount * exchangeRate
+  } else {
+    toAmount = amount
+    fromAmount = amount / exchangeRate
+  }
+
+  useEffect(() => {
+    fetch(LatestRates)
+      .then(response => response.json())
+      .then(data => {
+        const firstCurrency = Object.keys(data.rates)[0]
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+        setFromCurrency(data.base)
+        setToCurrency(firstCurrency)
+        setExchangeRate(data.rates[firstCurrency])
+      })
+  }, [])
+
+  function handleFromAmountChange(e) {
+    setAmount(e.target.value)
+    setAmountInFromCurrency(true)
+  }
+
+  function handleToAmountChange(e) {
+    setAmount(e.target.value)
+    setAmountInFromCurrency(false)
+  }
+
   return (
     <div className="App">
       <h1 className="title">Quick Conversion</h1>
-      <QCRow />
+      <QCRow 
+        currencyOptions={currencyOptions}
+        selectedCurrency={fromCurrency}
+        onChangeCurrency={e => setFromCurrency(e.target.value)}
+        onChangeAmount={handleFromAmountChange}
+        amount={fromAmount}
+      />
       <div className="qc-middle-box">
         <div className="equals">=</div>
         <div className="swap-button">
           <button>Swap</button>
         </div>
       </div>
-      <QCRow />
+      <QCRow 
+        currencyOptions={currencyOptions}
+        selectedCurrency={toCurrency}
+        onChangeCurrency={e => setToCurrency(e.target.value)}
+        onChangeAmount={handleToAmountChange}
+        amount={toAmount}
+      />
     </div>
   );
 }
